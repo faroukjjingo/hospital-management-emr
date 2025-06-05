f448e4dab9c5c22e81f097d1f9c732b1f20d1a52
Jjingo Farouk <jjingofarouq@gmail.com>
2025-06-04 17:47:37 -0700

    Update main.ts

--- Code/Websites/DanpheEMR/wwwroot/DanpheApp/src/main.ts
+++ Code/Websites/DanpheEMR/wwwroot/DanpheApp/src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
 import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(APP_ROUTES)
+  ]
+}).catch(err => console.error(err));
